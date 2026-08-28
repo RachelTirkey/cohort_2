172b3310841e76d61353c7b24ab7b166c4f1d499
Rachel Tirkey <racheltirkey.in@gmail.com>
2026-08-28 07:10:14 +0530

    practice put route

--- week-2/week-2.5/index.js
+++ week-2/week-2.5/index.js
@@ -71,6 +71,15 @@ app.post("/", function (req, res) {
     })
 })
 
+app.put("/", function (req, res) {
+    for (let i = 0; i < users[0].kidneys.length; i++) {
+        users[0].kidneys[i].healthy = true;
+    }
+    res.json({
+        
+    })
+})
+
 app.listen(3000);
 
 
