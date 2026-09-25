4292b6a0beebf5ff91b010c9078aea010a039ef4
Rachel Tirkey <racheltirkey.in@gmail.com>
2026-09-25 15:14:13 +0530

    Build counter app

---
+++ week-5/week-5.1/counter-app/src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+
+function App() {
+  const [todos, setTodos] = useState([{
+    title: "Go to gym",
+    description: "Go to gym from 7-9",
+    completed: false
+   },
+   {
+    title: "Study DSA",
+    description: "Study DSA from 9 - 11",
+    completed: true
+   },
+   {
+    title: "Study DSA",
+    description: "Study DSA from 9 - 11",
+    completed: true
+   }
+]);
+  
+
+function addTodo(todo) {
+  setTodos([...todos, {
+    title: "new todo",
+    description: "desc of new todo"
+  }])
+}
+
+  return (
+     <div>
+       <button onClick={addTodo}>Add a random Todo</button>
+       {todos.map(function(todo){
+         return <Todo title={todo.title} description={todo.description}/>
+       })}
+     </div>
+  )
+}
+
+
+
+
+function Todo(props){
+
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      <p>{props.description}</p>
+    </div>
+  );
+}
+export default App
